24a2561e2f00e42fadfb30b099770d2958b7f2a4
Azat <azekeoktl@gmail.com>
2024-04-21 20:15:12 +0500

    added getbookbyid

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
-	Id   int    `json:"id"`
+	Id   string `json:"id"`
 	Name string `json:"name"`
 }
 
 var Books = []Book{
-	{Id: 1, Name: "1984"},
+	{Id: "1", Name: "1984"},
 }
 
 func RetriveBooks(c *gin.Context) {
@@ -29,10 +30,34 @@ func AddBook(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, NewBook)
 }
 
+func GetBook(c *gin.Context) {
+	id := c.Param("id")
+	book, err := GetBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+
+}
+
+func GetBookById(id string) (*Book, error) {
+	for i, t := range Books {
+		if t.Id == id {
+			return &Books[i], nil
+		}
+	}
+
+	return nil, errors.New("to do not found")
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/Books", RetriveBooks)
 	r.POST("/Books", AddBook)
+	r.GET("/Books/:id", GetBook)
 	r.Run("localhost:8080")
 
 }
